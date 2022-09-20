b62c72aee925b07232bc1adb85854dff03cac7c3
Kiransingavarapu <18pa1a05e8@vishnu.edu.in>
2022-09-21 05:10:59 +0530

    Iterate through all elements in a array list

---
+++ src/color1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class color1 {
+    public static void main(String[] args) {
+        List<String> list_Strings = new ArrayList<String>();
+        list_Strings.add("Black");
+        list_Strings.add("Purple");
+        list_Strings.add("White");
+        list_Strings.add("Gray");
+        for (String hi : list_Strings) {
+            System.out.println(hi);
+        }
+    }
+}
